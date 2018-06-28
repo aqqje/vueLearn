13e416f7c4d8afd41fb6bdccb32ea047410bc1d1
aqqje <1042136232@qq.com>
2018-06-28 22:39:19 +0800

    201806282239

--- VueDemo/src/main.js
+++ VueDemo/src/main.js
@@ -1,13 +1,3 @@
 /*
-入口 JS：创建 Vue 实例
-*/
-import Vue from 'vue'
-import App from './App.vue'
-
-new Vue({
-  el: '#app',
-  components:{
-    App
-  },
-  template: '<App/>'
-})
+*  入口 JS: 创建 Vue 实例
+* */
