f79cab02d6d6d41c00ff79c65e24f8c0de49a536
aqqje <1042136232@qq.com>
2018-06-30 20:32:01 +0800

    201806302031

---
+++ VueDemo/src-mint-ui/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <mt-button  @click.native="handleClick" type="primary" style="width: 100%">LOVES</mt-button>
+</template>
+
+<script>
+  import { Toast } from 'mint-ui';
+    export default {
+      methods:{
+        handleClick(){
+          Toast('LIULI');
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
--- VueDemo/src/App.vue
+++ VueDemo/src/App.vue
@@ -1,16 +1,8 @@
 <template>
-  <mt-button  @click.native="handleClick" type="primary" style="width: 100%">LOVES</mt-button>
 </template>
 
 <script>
-  import { Toast } from 'mint-ui';
-    export default {
-      methods:{
-        handleClick(){
-          Toast('LIULI');
-        }
-      }
-    }
+
 </script>
 
 <style>
